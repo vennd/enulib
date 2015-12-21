88f7a2313e260afbe5e65c476a764bee53451ed0
whoisjeremylam <jeremy@vennd.io>
2015-12-22 08:52:02 +0900

    Add get asset request sample

--- enulibgo/samples/getassetrequest/getassetrequest.go
+++ enulibgo/samples/getassetrequest/getassetrequest.go
@@ -1,22 +1,45 @@
 package main
 
 import (
-	"github.com/vennd/enulib/enulibgo"
+	"os"
 	"log"
+	"encoding/json"
+	
+	"github.com/vennd/enulib/enulibgo"
 )
 
+func usage() {
+	println("Usage:\n")
+	println("getassetrequest <assetRequestId>")
+	println("")
+	println("Where <assetRequestId> is the assetRequestId returned after performing an asset creation request.")
+}
+
 func main() {
+	// One parameter must be specified which is the assetId
+	if len(os.Args) != 2 {
+		usage()
+		
+		os.Exit(1)
+	}
+	
+	assetId := os.Args[1]
+
 	enulibgo.Init()
 
 	// Set values for asset
-	var assetId = "cd91fb5593e5336453fb454041149a67"
+//	var assetId = "46e24f05d7b7bbffbe30f65a729b5823"
 
 	asset, err := enulibgo.GetAsset(assetId)
-
 	if err != nil {
 		log.Printf("Failed to get asset. (Request Id: %s)", asset.RequestId)
 		log.Fatal(err)
 	}
-
-	log.Printf("Reply: %+#v", asset)
+	
+	prettyPrint, errMarshal := json.MarshalIndent(asset, "", "  ")
+	if errMarshal != nil {
+		log.Panic(errMarshal)
+	}
+	
+	log.Println(string(prettyPrint))
 }
