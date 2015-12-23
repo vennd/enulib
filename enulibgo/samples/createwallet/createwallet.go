b58d2f679ec653656810a39a69cac2b567ee44f5
whoisjeremylam <jeremy@vennd.io>
2015-12-24 06:42:04 +0900

    Update createwallet sample

--- enulibgo/samples/createwallet/createwallet.go
+++ enulibgo/samples/createwallet/createwallet.go
@@ -6,27 +6,31 @@ import (
 	"github.com/vennd/enulib/enulibgo"
 )
 
-var blockchains = []string{
-	"counterparty",
-	"ripple",
-	"gammagammazero",
+type blockchainWallet struct {
+	Blockchain       string
+	AddresesToCreate uint64
+}
+
+var blockchains = []blockchainWallet{
+	{"counterparty", 20},
+	{"ripple", 0},
+	{"gammagammazero", 14},
 }
 
 func main() {
 	enulibgo.Init()
 
 	for _, blockchain := range blockchains {
-		log.Printf("Creating %s wallet", blockchain)
-		wallet, err := enulibgo.CreateWallet(blockchain)
+		log.Printf("Creating %s wallet and requesting %d addresses\n", blockchain.Blockchain, blockchain.AddresesToCreate)
+		wallet, err := enulibgo.CreateWallet(blockchain.Blockchain, blockchain.AddresesToCreate)
 
 		if err != nil {
-			log.Fatal(err.Error())
+			log.Println(err.Error())
 		} else {
-			log.Printf("%s wallet creation successful. Wallet passphrase created: %s (Request Id: %s)", blockchain, wallet.Passphrase, wallet.RequestId)
+			log.Printf("%s wallet creation successful. Wallet passphrase created: %s (Request Id: %s)", blockchain.Blockchain, wallet.Passphrase, wallet.RequestId)
 
 			for i, address := range wallet.Addresses {
 				log.Printf("Wallet address %d: %s\n", i, address)
-
 			}
 		}
 
